586e3a68d9d2e03e41607236f42a384c1520f77b
Fernando Toledo <ftoledo@docksud.com.ar>
2015-07-15 22:59:12 -0300

    template para compartir ficha en facebook

--- server/src/PressEnter/MatematiconBundle/Controller/DefaultController.php
+++ server/src/PressEnter/MatematiconBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace PressEnter\MatematiconBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use PressEnter\MatematiconBundle\Entity\Drawing;
 
 class DefaultController extends Controller
 {
@@ -13,10 +14,26 @@ class DefaultController extends Controller
             'user' => $this->getUser()
         ));
     }
-    
+
     public function downloadAction(Request $request)
     {
         return $this->render('PressEnterMatematiconBundle:Default:download.html.twig', array(
         ));
     }
+
+    /**
+     *  publica una ficha de objecto
+     */
+    public function objectAction($item = '')
+    {
+      $em = $this->getDoctrine()->getManager();
+      $drawing = $em->getRepository('PressEnterMatematiconBundle:Drawing')->find($item);
+      if(!$drawing)
+      {
+          throw $this->createNotFoundException('Unable to find Drawing entity.');
+      }
+
+      return $this->render('PressEnterMatematiconBundle:Default:object_share.html.twig', array('item'=> $item, 'drawing' => $drawing));
+    }
+
 }
